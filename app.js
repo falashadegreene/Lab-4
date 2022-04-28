19e0431cf39484c0d0693d53c6c9758b0e73d8ca
Falashade Greene <falashadegreene@192.168.1.6>
2022-04-28 09:34:32 -0400

    added app.js file

---
+++ app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let userName = prompt ("What is your name?");
+
+console.log (userName);
+
+if (userName === "falashade") {
+    console.log("Hey there nomad!");
+} else {
+    console.log ("Hi! Welcome, Traveler!");
+}
+
