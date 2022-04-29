6260e391b825f6c35ff00a5fd450c15b92eaa585
Falashade Greene <falashadegreene@192.168.1.6>
2022-04-29 17:11:41 -0400

    added functionss and response error

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 'use strict';
-// declare a variable, assign it the value of the user's input
-let userName = prompt ("What is your name?");
-// print the user's input to the console
-console.log (userName);
 
-if (userName === "falashade") {
-    console.log("Hey there nomad!");
-} else {
-    console.log ("Hi! Welcome, Traveler!");
+function greeting () {
+
+    let userName = prompt ("What is your name?");
+    console.log (userName);
+    
+    
+    if (userName === "falashade") {
+        console.log("Hey there nomad!");
+    } else {
+        console.log ("Hi! Welcome, Traveler!");
+    } 
+    
+    
+    document.write("<p id='person-name'> " + userName + "</p>");
+
+}
+greeting ()
+
+function myTravel () {
+
+    let userResponse = prompt ("Where do you want to travel?") ;
+    console.log ("a country ")
+
+    while (userResponse != "a country") {
+    userResponse = prompt ("pick something else")
+    }
 }
 
-// how can I put data on the page?
-document.write("<p id='person-name'> " + userName + "</p>");
+
+
+
+
+
+
